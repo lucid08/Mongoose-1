24c4700317a8c4be359f056862531a9dcb22ae54
Sumit Choudhary <sumit.rc619@gmail.com>
2025-05-19 02:56:48 +0530

    First commit

---
+++ models/order.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    productIds: [
+      { type: Schema.Types.ObjectId, ref: "Product", required: true },
+    ],
+    status: {
+      type: String,
+      enum: ["pending", "shipped", "delivered", "cancelled"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
---
+++ models/product.model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  stock: { type: Number, required: true },
+  category: String,
+  vendorId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+},   { timestamps: true });
+
+module.exports = mongoose.model("Product", productSchema);
